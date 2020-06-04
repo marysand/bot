7421ed255963622abad422a8fb64a893f0f82d8d
Mary_s_and <andreeva.maria.s@gmail.com>
2020-06-04 15:29:46 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,6 @@
-TOKEN = "1116521412:AAFMU3n97_kO3RlIgyMWgxgdj79IqLHaJto"
+TOKEN = ""
 
 PROXY = {
-    'https': 'Socks5://210478695:sQuculni@ss-01.s5.ynvv.cc:999',
-    'Socks5': 'Socks5://210478695:sQuculni@ss-01.s5.ynvv.cc:999'
+    
 }
 
